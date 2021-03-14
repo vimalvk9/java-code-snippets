c4b533d0f17c45f64a0a91af94df7c8e9e48ede6
vimalkumar9 <vimal.kumar@vedantu.com>
2021-03-14 12:57:19 +0530

    add -- new solutions added

---
+++ ArrayStringsAreEqual.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;public class ArrayStringsAreEqual {
+    
+    public static void main(String args []) {
+        System.out.println("log here");
+    }
+}
+
+
---
+++ XorOperation.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;public class XorOperation {
+    
+    public static void main(String args []) {
+        System.out.println("log here");
+    }
+}
+
+
